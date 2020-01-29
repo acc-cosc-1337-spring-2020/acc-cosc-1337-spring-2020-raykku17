2ed3d6a5ab13dae13490f91764977bc241867b0e
9418925 <yesy.castro@g.austincc.edu>
2020-01-28 19:04:21 -0600

    avoid, returns and created tests

--- src/examples/01_module/02_variables/vars.cpp
+++ src/examples/01_module/02_variables/vars.cpp
@@ -1,1 +1,15 @@
+#include "vars.h"
+#include<iostream>//use standard library code/object
+using std::cout;
 //cpp
+
+void display_number(int num)
+{
+	cout << num << "\n";
+}
+
+int square_number(int num)
+{
+	return num*num;
+}
+
--- src/examples/01_module/02_variables/vars.h
+++ src/examples/01_module/02_variables/vars.h
@@ -1,1 +1,7 @@
 //header
+#include<iostream>//use standard library code/object
+using std::cout;
+
+void display_number(int num);
+
+int square_number(int num);
