9ad9283faeb0eaed9b5a77dc06680e01aea1c87b
9418925 <yesy.castro@g.austincc.edu>
2020-01-28 19:22:47 -0600

    Homework 1

--- test/homework_test/01_expressions_test/01_expressions_tests.cpp
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -12,3 +12,10 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("verify multiply_numbers function")
+{
+	REQUIRE(multiply_numbers(5, 5) == 25);
+	REQUIRE(multiply_numbers(10, 10) == 100);
+	REQUIRE(multiply_numbers(6, 6) == 36);
+
+}
