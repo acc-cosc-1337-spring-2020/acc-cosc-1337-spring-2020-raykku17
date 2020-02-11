fc349a1d3b906b5cabadad48be8993bb68db252a
Yesy <yesy.castro@g.austincc.edu>
2020-02-11 14:06:26 -0600

    Worked on creating test cases for homework 2 assignment

---
+++ out/build/x64-Debug/test/homework_test/02_decisions_test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/test/homework_test/02_decisions_test
+# Build directory: C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/out/build/x64-Debug/test/homework_test/02_decisions_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hw_02_tests "C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/out/build/x64-Debug/test/homework_test/02_decisions_test/hw_02_tests.exe" "-s")
+set_tests_properties(hw_02_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/test/homework_test/02_decisions_test/CMakeLists.txt;3;add_test;C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/test/homework_test/02_decisions_test/CMakeLists.txt;0;")
---
+++ out/build/x64-Debug/test/homework_test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/test/homework_test
+# Build directory: C:/Users/Dylan/source/repos/acc-cosc-1337-spring-2020-raykku17/out/build/x64-Debug/test/homework_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("02_decisions_test")
