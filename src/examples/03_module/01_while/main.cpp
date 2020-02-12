cbce69fbaa52951bf83bb78c523142321c45fc84
9418925 <yesy.castro@g.austincc.edu>
2020-02-11 19:01:44 -0600

    While loop for square numbers; worked on displaying numbers using for; worked on do_while loop

--- src/examples/03_module/01_while/main.cpp
+++ src/examples/03_module/01_while/main.cpp
@@ -1,7 +1,18 @@
 #include "while.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	int num, sum;
+	cout << "Enter a number: ";
+	cin >> num;
+
+	sum = sum_of_squares(num);
+
+	cout << "Sum of Squares for"  <<  num  <<  "is"  <<  sum;
+
 
 	return 0;
 }
