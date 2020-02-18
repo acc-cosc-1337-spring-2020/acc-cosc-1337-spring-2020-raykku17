3fd80c6fd0a0eee04108483cb82611f01e148474
Yesy <yesy.castro@g.austincc.edu>
2020-02-18 17:05:29 -0600

    Began work on hw3

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -1,4 +1,10 @@
 #include "dna.h"
+#include <iostream>
+#include<vector>
+
+using std::cout; 
+using std::cin;
+using namespace std;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,6 +12,27 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna)
+{
+	int count = 0;
+	double gc = 0;
+	double dnalength = dna.length();
+	for (int i = 0; i < dna.length(); i++)
+	{
+		if (dna.at(i) == 'G')
+		{
+			count++;
+		}
+		else if (dna.at(i) == 'C')
+		{
+			count++;
+		}
+	}
+	gc = count / dnalength;
+
+	return gc;
+}
+
 
 
 
@@ -14,6 +41,19 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+
+string get_reverse_string(string dna)
+{
+	string rev;
+	for (int i = dna.size() - 1; i >= 0; i--)
+	{
+		rev = rev.append(1, dna[i]);
+	}
+	return rev;
+
+
+}
+
 
 
 
@@ -28,3 +68,23 @@ c. return string
 
 */
 
+string get_dna_complement(string dna)
+{
+	string A;
+	string T;
+	string G;
+	string C;
+
+	do
+	{
+		string dna;
+		cout << "Enter dna sequence: ";
+		cin >> dna;
+		cout << (get_reverse_string(dna));
+
+		cout << "complement is: ";
+		cin >> A; T; C; G;
+	} while ((string(A) == T)); (string(T) == A); (string(G) == C); (string(C) == G);
+
+	return string();
+}
