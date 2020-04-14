af397618b6fa11119e4dce7ebabd42724b9e0366
Yesy <yesy.castro@g.austincc.edu>
2020-04-14 18:51:16 -0500

    Classwork 9

--- src/examples/09_module/temperature_data.cpp
+++ src/examples/09_module/temperature_data.cpp
@@ -1,1 +1,39 @@
+#include "temperature_data.h"
+
 //temperature_data.cpp
+
+void TemperatureData::save_temps(std::vector<Temperature>& ts)
+{
+	std::ofstream file_out(file_name, std::ios_base::trunc);
+
+	for (auto temp : ts)
+	{
+		file_out << temp.get_hour();
+		file_out << " ";
+		file_out << temp.get_reading();
+		file_out << "\n";
+	}
+	file_out.close();
+}
+
+std::vector<Temperature> TemperatureData::get_temps() const
+{
+	std::vector<Temperature>temps;
+	std::ifstream read_file(file_name);
+
+	int hour;
+	double reading;
+
+	if (read_file.is_open()) //if the file is open
+	{
+		while (read_file >> hour >> reading) //gets file, then hour, then reading
+		{
+			Temperature temp(hour, reading); //reads these
+			temps.push_back(temp); //adds to vector
+		}
+	}
+	read_file.close();
+
+	return temps;
+
+}
--- src/examples/09_module/temperature_data.h
+++ src/examples/09_module/temperature_data.h
@@ -1,1 +1,22 @@
 //temperature_data.h
+#include "temperature.h"
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+#ifndef TEMPERATURE_DATA_H
+#define TEMPERATURE_DATA_H
+
+class TemperatureData
+{
+public:
+	void save_temps(std::vector<Temperature>&ts);
+	std::vector<Temperature>get_temps()const;
+
+private:
+	const std::string file_name{ "temperature.dat" };
+};
+
+
+#endif // !TEMPERATURE_DATA_H
