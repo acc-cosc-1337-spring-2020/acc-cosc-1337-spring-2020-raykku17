ac252845eed3145b23dc91ca5a0b888f68f9ab43
Yesy <yesy.castro@g.austincc.edu>
2020-04-16 19:03:01 -0500

    memory leak

--- src/examples/11_module/02_dynamic_memory/main.cpp
+++ src/examples/11_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,17 @@
-int main() 
+#include <iostream>
+
+using std::cout;
+
+int main()
 {
-	
+	int* ptr_num = new int(5); //new is required to create dynamic memory
+
+	cout << "Address of ptr_num: " << &ptr_num << "\n";
+	cout << "Address ptr_num points to: " << ptr_num << "\n";
+	cout<<"Value at address ptr_num points to: "<<*ptr_num<<"\n\n";
+
+	delete ptr_num; //after creating memory you must delete it, its essentially cleaning up after yourself
+	ptr_num = nullptr; //zeroes out ptr_num
+
 	return 0;
 }
