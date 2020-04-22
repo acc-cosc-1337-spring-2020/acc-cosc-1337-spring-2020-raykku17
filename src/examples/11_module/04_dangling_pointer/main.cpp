a81d54f9edbc9cb3f664d2fd2fde1e481e138833
Yesy <yesy.castro@g.austincc.edu>
2020-04-21 19:12:40 -0500

    copy memory

--- src/examples/11_module/04_dangling_pointer/main.cpp
+++ src/examples/11_module/04_dangling_pointer/main.cpp
@@ -1,8 +1,17 @@
 #include "dangling_pointer.h"
+#include <iostream>
 
 int main() 
 {
-	
+	int* ptr_num = new int(5);
+	int* ptr = ptr_num;
+
+	std::cout << *ptr << "\n";;
+
+	delete ptr_num;
+	ptr_num = nullptr;
+
+	std::cout << *ptr;
 
 	return 0;
 }
--- test/classwork_test/11_assign_test/vector_tests.cpp
+++ test/classwork_test/11_assign_test/vector_tests.cpp
@@ -6,3 +6,23 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test class copy with stack variable")
+{
+	Vector v1(3);
+	Vector v2 = v1;
+
+	REQUIRE(v1.Size() == v2.Size());
+}
+
+TEST_CASE("Test class copy with dynamic memory(heap) variables")
+{
+	Vector v1(3);
+	Vector v2 = v1;
+
+	REQUIRE(v1.Size() == v2.Size());
+	v1[1] = 5;
+
+	REQUIRE(v1[1] != v2[1]);
+	REQUIRE(v1[1] == 5);
+	REQUIRE(v2[1] == 0);
+}
