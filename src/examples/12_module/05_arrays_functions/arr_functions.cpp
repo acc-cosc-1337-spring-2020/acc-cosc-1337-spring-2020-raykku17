da6320b163171f0f59d11dc35c2a71fb52f9ac71
Yesy <yesy.castro@g.austincc.edu>
2020-05-07 18:50:09 -0500

    array size parameters

--- src/examples/12_module/05_arrays_functions/arr_functions.cpp
+++ src/examples/12_module/05_arrays_functions/arr_functions.cpp
@@ -2,3 +2,11 @@
 #include<iostream>
 #include "arr_functions.h"
 //define iterate_array and loop through with ++ increment
+
+void iterate_array(int * hours, const int SIZE)
+{
+	for (int i = 0; i < SIZE; ++i)
+	{
+		std::cout << hours[i] << "\n";  //the i displays each element in hours
+	}
+}
--- src/examples/12_module/05_arrays_functions/main.cpp
+++ src/examples/12_module/05_arrays_functions/main.cpp
@@ -2,7 +2,10 @@
 
 int main() 
 {
-	
+	const int SIZE = 5;
+	int hours[SIZE] = { 0,10,20,30,40 };
 
+	iterate_array(hours, SIZE);  //array does not have a sense of size, you must declare it
+	
 	return 0;
 }
