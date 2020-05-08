228d0a8ca75f215d01496b4e9d47a41d286332a8
Yesy <yesy.castro@g.austincc.edu>
2020-05-07 19:13:17 -0500

    char array

--- src/examples/12_module/07_arrays_dyn_ch/arrays_dn_ch.cpp
+++ src/examples/12_module/07_arrays_dyn_ch/arrays_dn_ch.cpp
@@ -1,1 +1,15 @@
-//cpp
+#include "arrays_dyn_ch.h"
+#include <iostream>
+
+void dynamic_array()
+{
+	const int SIZE = 10;
+	char* name = new char[SIZE]; //ALLOCATES
+	std::cout << "Enter name: ";
+	std::cin.getline(name, SIZE);  //CAPTURES from keyboard
+
+	std::cout << name << "\n";
+
+	delete[]name;  //you must delete to prevent memory leaks
+
+}
--- src/examples/12_module/07_arrays_dyn_ch/main.cpp
+++ src/examples/12_module/07_arrays_dyn_ch/main.cpp
@@ -1,8 +1,9 @@
-
+#include "arrays_dyn_ch.h"
 //dynamic array allocation and deallocation
 
 int main()
 {
-	
+	dynamic_array();
+
 	return 0;
 }
